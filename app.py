072e283155a9f9144ff876bb2bae6256045f4f21
acco000 <k338115@kansai-u.ac.jp>
2021-08-19 16:44:31 +0900

    make it better

--- app.py
+++ app.py
@@ -230,7 +230,8 @@ def handle_message(event):
                 'http://syllabus3.jm.kansai-u.ac.jp/syllabus/search/ref/1/7/11/171190.html', # 136 社会調査実習
                 ]
 #================================シラバス================================================================================
-    #soup = BeautifulSoup(r.content, "html.parser") #soup.find(id="kamoku").text found  = 0
+    #soup = BeautifulSoup(r.content, "html.parser") #soup.find(id="kamoku").text 
+    found  = 0
     j = 0
     for i in url_array:
         j = j + 1
@@ -247,7 +248,7 @@ def handle_message(event):
         elif j == len(url_array):
             break
     if  found == 1:
-        reply_message = f"その科目の評価方法は,\n「{sps}」です.\n{url}"
+        reply_message = f"その科目の評価方法は,\n「{sps}」\nです.\n{url}"
     else:
         reply_message = f"すみません. \n関大総情秋学期の講義にのみ対応しています. もう一度送信内容をご確認ください."
 
